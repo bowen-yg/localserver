46f9c1d151ca5d7547deaf5972631147c72b0153
bowen <693015917@qq.com>
2024-09-16 22:35:56 +0800

    dev

--- upload.py
+++ upload.py
@@ -1,25 +1,42 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 import time
+import cgi
+
 # 创建自定义的请求处理类
 class FileUploadHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        num=0
         start_time = time.time()
-        content_length = int(self.headers['Content-Length'])
-        # 读取客户端发送的二进制文件数据
-        file_data = self.rfile.read(content_length)
-        # 在这里可以对接收到的文件数据进行处理，例如保存到磁盘
-        with open('file.bin', 'wb') as file:
-            file.write(file_data)
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'File uploaded successfully.')
+        content_type, pdict = cgi.parse_header(self.headers['Content-Type'])
+        
+        if content_type == 'multipart/form-data':
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            pdict['CONTENT-LENGTH'] = int(self.headers['Content-Length'])
+            fields = cgi.parse_multipart(self.rfile, pdict)
+            uploaded_file = fields.get('file')[0]
+            file_name = fields.get('file')[1]
+            
+            # 创建存储上传文件的目录
+            upload_dir = 'uploaded_files'
+            if not os.path.exists(upload_dir):
+                os.makedirs(upload_dir)
+            
+            # 保存文件到指定目录
+            file_path = os.path.join(upload_dir, file_name)
+            with open(file_path, 'wb') as file:
+                file.write(uploaded_file)
+            
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'File uploaded successfully.')
+        else:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write(b'Invalid content type.')
 
         end_time = time.time()
         time_elapsed_ms = int((end_time - start_time) * 1000)
         print(f"Update in {time_elapsed_ms} ms")
-
 
 # 启动服务器
 def run_server():
@@ -27,7 +44,6 @@ def run_server():
     httpd = HTTPServer(server_address, FileUploadHandler)
     print('Server running on port 8000...')
     httpd.serve_forever()
-
 
 # 运行服务器
 run_server()
