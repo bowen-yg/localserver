8e1d030ae815b2ca54aa07c169d2511d4d5fe5d3
john-yue <ooo.c.f.yue.ooo@gmail.com>
2024-09-17 14:41:09 +0800

    func(feature): 添加功能
    1. 多文件上传功能;
    2. 可上传重复文件功能;
    3. 显示已上传文件功能;
    
    Changes to be committed:
    	new file:   .gitignore 可以忽略不想上传的文件
    	modified:   readme.md 修改说明
    	new file:   requirements.txt 列出程序启动需要的依赖

--- upload.py
+++ upload.py
@@ -1,35 +1,40 @@
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, send_from_directory
 import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './uploaded_files/'
 
-
-UPLOAD_FOLDER = './uploaded_files/'  # 指定上传文件夹
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'md', 'py', 'js', 'java', 'c', 'h', 'mp4'}  # 允许的文件扩展名
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'md', 'py', 'js', 'java', 'c', 'h', 'mp4'}
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def get_uploaded_files():
+    files = []
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+            files.append(filename)
+    return files
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    uploaded_files = get_uploaded_files()
     if request.method == 'POST':
-        # 检查是否有文件在请求中
-        if 'file' not in request.files:
-            return '没有文件部分'
-        file = request.files['file']
-        # 如果用户没有选择文件，浏览器也会
-        # 提交一个没有文件名的空部分
-        if file.filename == '':
-            return '没有选择文件'
-        if file and allowed_file(file.filename):
-            filename = file.filename
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            return '文件上传成功'
-        else:
-            return '不允许的文件类型'
-    return render_template('./index.html')
+        for file in request.files.getlist('file'):
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                return '不允许的文件类型'
+        uploaded_files = get_uploaded_files()  # 更新文件列表
+    return render_template('index.html', files=uploaded_files, allowed_extensions=ALLOWED_EXTENSIONS)
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(host='0.0.0.0', port=8080)
